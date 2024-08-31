04b83f18087eb778763ac58fe24b39b65df934a5
Your Name <130116701+royfrias@users.noreply.github.com>
2024-08-31 19:58:21 -0400

    more updates to App.js

--- server/App.js
+++ server/App.js
@@ -273,9 +273,6 @@ app.get("/channel-users", async (req, res) => {
   }
 });
 
-app.listen(process.env.SERVER_PORT, () => {
-  console.log(`Server is now listening on port ${process.env.SERVER_PORT}`);
-});
 // app.use('/api/s3', s3Router)
 
 // app.listen(config.PORT, () => {
