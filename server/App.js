c11a0f1abe3a545c9f80125b3432b7655ec39a88
Your Name <130116701+royfrias@users.noreply.github.com>
2024-08-31 20:10:21 -0400

    new update to dist folder

--- server/App.js
+++ server/App.js
@@ -90,14 +90,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 app.use("/", router);
 app.use("/user", userRouter);
 app.use("/admin", adminRouter);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
 AWS.config.update({
