eeeaed20bdaad454d2f8f43b34ccbedc9b1a9047
Your Name <130116701+royfrias@users.noreply.github.com>
2024-09-01 14:30:30 -0400

    app.js change

--- server/App.js
+++ server/App.js
@@ -81,8 +81,6 @@ const fetchUserMap = async () => {
 
 fetchUserMap(); // Fetch user map initially
 
-const pathToDist = path.join(__dirname, "..", "client", "dist");
-
 const app = express();
 // const prisma = new PrismaClient()----MO
 
@@ -91,20 +89,14 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "client", "dist")));
 app.use("/", router);
 app.use("/user", userRouter);
 app.use("/admin", adminRouter);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.use(express.static(pathToDist));
-
 app.get("*", (req, res) => {
-  res.sendFile(path.join(pathToDist, "index.html"), (err) => {
-    if (err) {
-      console.error("Error sending file:", err);
-      res.status(404).send("Page not found");
-    }
-  });
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
 AWS.config.update({
