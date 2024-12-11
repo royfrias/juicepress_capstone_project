1c80da778f2ecb593e3c028e2e7f7cfdf1053742
Your Name <130116701+royfrias@users.noreply.github.com>
2024-12-10 21:45:08 -0500

    changes to routes

--- server/App.js
+++ server/App.js
@@ -285,3 +285,7 @@ db.on("connected", () => {
 db.on("error", (err) => {
   console.log(err);
 });
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
--- server/db.js
+++ server/db.js
@@ -1,4 +1,13 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 import "dotenv/config";
 
-export const client = new MongoClient(process.env.ATLAS_CONNECTION)
+export const client = new MongoClient(process.env.ATLAS_CONNECTION);
+
+client
+  .connect()
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
