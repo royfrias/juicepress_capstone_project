99a2ba4fa846ad5bea10893397aa1502e8d026ac
Your Name <130116701+royfrias@users.noreply.github.com>
2024-12-15 15:24:34 -0500

    changes to routes signUp and server

--- server/controllers/routes-user.js
+++ server/controllers/routes-user.js
@@ -8,16 +8,15 @@ import userValidationMiddleware from "../middleware/validationMiddleware.js";
 const router = Router();
 
 //checks to see if user exists and if not adds new user to database
-router.post("/signup", async (request, response) => {
+router.post("/user/signup", async (request, response) => {
   try {
     //checking if user exists in database
     const UserExists = await User.exists({
-      employeeID: request.body.employeeID
-    })
+      employeeID: request.body.employeeID,
+    });
 
     //user does not exist in database, create new user
     if (UserExists === null) {
-
       //created variable for hashing password
       const hashedPassword = await bcryptjs.hash(request.body.password, 10);
 
@@ -25,22 +24,19 @@ router.post("/signup", async (request, response) => {
         firstName: request.body.firstName,
         lastName: request.body.lastName,
         password: hashedPassword,
-        employeeID: request.body.employeeID
-
+        employeeID: request.body.employeeID,
       });
-
 
       //save new user to database
       await user.save();
 
       //allowing user to access chat server
-      const token = jwt.sign({ id: user._id },
-        process.env.SECRET_KEY);
+      const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY);
 
       response.send({
         message: "User Successfully added!",
         token,
-        user
+        user,
       });
     } else {
       response.send("User already taken!");
@@ -50,26 +46,27 @@ router.post("/signup", async (request, response) => {
   }
 });
 
-
-
 // log user in and verify token
 router.post("/login", async (request, response) => {
   try {
     const user = await User.findOne({
-      employeeID: request.body.employeeID
+      employeeID: request.body.employeeID,
     });
 
-    if (user && await bcryptjs.compare(request.body.password, user.password)) {
+    if (
+      user &&
+      (await bcryptjs.compare(request.body.password, user.password))
+    ) {
       // User not found
       const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY);
       response.send({
         message: "Success",
-        token
+        token,
       });
     } else {
       response.status(401).send({
-        message: "Invalid username or password"
-      })
+        message: "Invalid username or password",
+      });
     }
   } catch (err) {
     response.status(500).send({ message: err.message });
@@ -79,11 +76,9 @@ router.post("/login", async (request, response) => {
 //displaying username and user pic on home pg after user logs in
 router.get("/username", userValidationMiddleware, (request, response) => {
   try {
-
-    response.send(request.user)
-
+    response.send(request.user);
   } catch (error) {
-    response.status(500).send({ message: err.message })
+    response.status(500).send({ message: err.message });
   }
 });
 
